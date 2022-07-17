37b1da8e86ece68b380a93b579321b71d7ab54e6
Jeremy Stark <razestudiosdev@gmail.com>
2022-07-17 15:11:29 -0400

    Began integrating firebase functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Telegram from './components/Telegram';
 import Sidebar from './components/Sidebar';
+import {auth} from './firebase';
+import {useSelector, useDispatch} from 'react-redux';
+import { selectUser, login, logout } from './features/counter/userSlice';
+import Login from './components/Login';
 
 function App() {
-  return (
-    <div className="app">
-        <Telegram/>
-    </div>
-  );
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+    auth.onAuthStateChange((authUser) => {
+        if(authUser)
+        {
+            dispatch(
+                login
+                ({
+                    uid: authUser.uid,
+                    photo: authUser.photoURL,
+                    email: authUser.email,
+                    displayName: authUser.displayName
+                })
+            )
+        } else {
+            dispatch(logout())
+        }
+    });
+    }, [dispatch])
+    return (
+        <div className="app">
+            {user ? <Telegram/> : <Login/>}
+        </div>
+    );
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 import { dblClick } from '@testing-library/user-event/dist/types/convenience';
-import firebase from 'firebase'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
