c2fbe0439050a9188541f57be683ebdc4218a7bc
Jeremy Stark <razestudiosdev@gmail.com>
2022-07-22 12:21:00 -0400

    Fixed error with useEffect method in Thread

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,15 +1,51 @@
 import { Avatar, IconButton } from '@material-ui/core'
 import { MicNone, MoreHoriz, SendRounded, Timer } from '@material-ui/icons'
-import React, { useState } from 'react'
-import './Thread.css'
+import React, { useEffect, useState } from 'react'
+import './Thread.css';
+import db from '../firebase';
+import firebase from 'firebase/compat/app';
+import { useSelector } from 'react-redux';
+import { selectThreadId, selectThreadName } from '../features/threadSlice';
+import { selectUser } from '../features/userSlice';
+import Message from './Message';
 
 const Thread = () => {
     const [input, setInput] = useState('');
+    const [messages, setMessages] = useState([]);
+    const threadName = useSelector(selectThreadName);
+    const threadId = useSelector(selectThreadId);
+    const user = useSelector(selectUser);
+
+    useEffect(() => {
+        if (threadId) {
+            db.collection('threads')
+                .doc(threadId)
+                .collection('messages')
+                .orderBy('timestamp', 'desc')
+                .onSnapshot((snapshot) => 
+                    setMessages(
+                        snapshot.docs.map(doc => ({
+                            id: doc.id,
+                            data: doc.data(),
+                        }))
+                    )
+                );
+        }
+    }, [threadId]);
+
     const sendMessage = (event) =>
     {
         event.preventDefault();
 
-        //firebase stuff here
+        db.collection('threads').doc(threadId).collection('messages').add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            uid: user.uid,
+            photo: user.photo,
+            email: user.email,
+            displayName: user.displayName
+        });
+        
         setInput('');
 
     }
@@ -28,7 +64,11 @@ const Thread = () => {
                 <MoreHoriz className='thread__header__details'/>
             </IconButton>
         </div>
-        <div className='thread__messages'></div>
+        <div className='thread__messages'>
+            {messages.map(({id, data}) => (
+                <Message key={id} data={data}/>
+            ))}
+        </div>
         <div className='thread__input'>
             <form>
                 <input placeholder='Write a message...' type='text' value={input} onChange={(e) => setInput(e.target.value)}></input>
